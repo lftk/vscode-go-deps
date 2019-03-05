12b05b84273ea4281b97bd6ea57a37bbaeb36bf9
zhoujun <dev.zhoujun@gmail.com>
2019-03-05 14:27:58 +0800

    init

---
+++ vscode-go-deps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export GO111MODULE=on
+export GOPROXY=https://mod.1byte.dev
+
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/acroca/go-symbols
+go get -u -v github.com/stamblerre/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v github.com/zmb3/gogetdoc
+go get -u -v golang.org/x/lint/golint
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v github.com/cweill/gotests/...
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/josharian/impl
+go get -u -v github.com/haya14busa/goplay/cmd/goplay
+go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+
+go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
